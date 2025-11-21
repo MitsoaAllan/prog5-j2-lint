92bbdd4f412265f68996237429852a989246278b
MitsoaAllan <hei.mitsoa@gmail.com>
2025-11-21 16:05:32 +0300

    test: the case of attribute amountVola into snake_case, CI should fail

--- Wallet.py
+++ Wallet.py
@@ -2,20 +2,20 @@ class Wallet:
     def __init__(self,color :str,size :int):
         self.color = color
         self.size = size
-        self.amountVola = 0
+        self.amount_vola = 0
     def open(self):
-        if self.amountVola == 0:
+        if self.amount_vola == 0:
             return "Wallet opened ,no money inside"
         return "Wallet opened, still poor"
-    def add_vola(self,amount : float):
-        self.amountVola+=amount
+    def addVola(self,amount : float):
+        self.amount_vola+=amount
         return "You added "+amount+"Ariary"
     def withdrawVola(self,amount : float):
-        self.amountVola-=amount
+        self.amount_vola-=amount
         return "You withdrawed "+amount+"Ariary"
     def close(self):
         return "Poor wallet closed"
     def isLost(self):
         return "Your wallet with no money is lost"
     def checkVola(self):
-        return self.amountVola
+        return self.amount_vola
