b7fce99d73421276b7c9aaa680681410e75e77c7
MitsoaAllan <hei.mitsoa@gmail.com>
2025-11-21 16:00:56 +0300

    test: the case of method open() into PascalCase, CI should fail

--- Wallet.py
+++ Wallet.py
@@ -2,20 +2,20 @@ class Wallet:
     def __init__(self,color :str,size :int):
         self.color = color
         self.size = size
-        self.AmountVola = 0
-    def open(self):
-        if self.AmountVola == 0:
+        self.amountVola = 0
+    def Open(self):
+        if self.amountVola == 0:
             return "Wallet opened ,no money inside"
         return "Wallet opened, still poor"
     def addVola(self,amount : float):
-        self.AmountVola+=amount
+        self.amountVola+=amount
         return "You added "+amount+"Ariary"
     def withdrawVola(self,amount : float):
-        self.AmountVola-=amount
+        self.amountVola-=amount
         return "You withdrawed "+amount+"Ariary"
     def close(self):
         return "Poor wallet closed"
     def isLost(self):
         return "Your wallet with no money is lost"
     def checkVola(self):
-        return self.AmountVola
+        return self.amountVola
