b5aa3631d720b528ea203dab5fc9ed279d659dde
MitsoaAllan <hei.mitsoa@gmail.com>
2025-11-21 16:07:11 +0300

    fix: remove all case problems, CI should pass

--- Wallet.py
+++ Wallet.py
@@ -2,20 +2,20 @@ class Wallet:
     def __init__(self,color :str,size :int):
         self.color = color
         self.size = size
-        self.amount_vola = 0
+        self.amountVola = 0
     def open(self):
-        if self.amount_vola == 0:
+        if self.amountVola == 0:
             return "Wallet opened ,no money inside"
         return "Wallet opened, still poor"
     def addVola(self,amount : float):
-        self.amount_vola+=amount
+        self.amountVola+=amount
         return "You added "+amount+"Ariary"
     def withdrawVola(self,amount : float):
-        self.amount_vola-=amount
+        self.amountVola-=amount
         return "You withdrawed "+amount+"Ariary"
     def close(self):
         return "Poor wallet closed"
     def isLost(self):
         return "Your wallet with no money is lost"
     def checkVola(self):
-        return self.amount_vola
+        return self.amountVola
