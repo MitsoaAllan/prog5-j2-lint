12c8954315f1d37837cea7ebce5443650a78c518
MitsoaAllan <hei.mitsoa@gmail.com>
2025-11-21 15:59:29 +0300

    test: the case of attributes amounVola into PascalCase, CI should fail

--- Wallet.py
+++ Wallet.py
@@ -1,21 +1,21 @@
-class wallet:
+class Wallet:
     def __init__(self,color :str,size :int):
         self.color = color
         self.size = size
-        self.amountVola = 0
+        self.AmountVola = 0
     def open(self):
-        if self.amountVola == 0:
+        if self.AmountVola == 0:
             return "Wallet opened ,no money inside"
         return "Wallet opened, still poor"
     def addVola(self,amount : float):
-        self.amountVola+=amount
+        self.AmountVola+=amount
         return "You added "+amount+"Ariary"
     def withdrawVola(self,amount : float):
-        self.amountVola-=amount
+        self.AmountVola-=amount
         return "You withdrawed "+amount+"Ariary"
     def close(self):
         return "Poor wallet closed"
     def isLost(self):
         return "Your wallet with no money is lost"
     def checkVola(self):
-        return self.amountVola
+        return self.AmountVola
